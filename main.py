10439d533f390629c875eed36eeae38682fbbbd6
Shrawan Devkota <52040084+SARWANdev@users.noreply.github.com>
2025-11-05 14:38:36 +0100

    Fix variable name for line sensor in LineFollower initialization

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from line_follower import LineFollower
 left_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.B)
 gyro_sensor = GyroSensor(Port.S1)
-line_sensor = ColorSensor(Port.S2)
+color_sensor = ColorSensor(Port.S2)
 ev3 = EV3Brick()
 wheel_diameter = 36   #Wheel Diameter
 axle_track = 120     #distance between wheels
@@ -17,7 +17,7 @@ steering = 60 #turn speed in °/s
 overshoot = 5 
 
 drive_base = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
-line_follower = LineFollower(drive_base=drive_base, line_sensor=line_sensor, drive_speed=10, black=9, white=85, blue=60)
+line_follower = LineFollower(drive_base=drive_base, line_sensor=color_sensor, drive_speed=10, black=9, white=85, blue=60)
 line_follower.avoid_obstacle()
 drive_base.straight(500)
 drive_base.turn(180)
