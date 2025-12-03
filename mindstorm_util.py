0f4b2ba62d44be18e4a9f59f98bbc3657eef9af0
Cornelius <uujud@student.kit.edu>
2025-12-03 16:43:15 +0100

    change color detection in mindstorm_util to tripel rgb

--- mindstorm_util.py
+++ mindstorm_util.py
@@ -13,7 +13,7 @@ class MindsStormUtil:
         drive backwards until touching the wall or max_counter of cm
         :param precision_module: Module to use for precisely drive backwards
         :param touch_sensor: Sensor to determine contact with wall
-        :param max_mm: Maximal amount of mm before aborting and returning false (accurate to 5mm)
+        :param max_mm: Maximal amount of mm before aborting and returning false
         :return: Returns true if wall was reached, false if max_cm was reached
         """
         ALIGN_BACKWARDS = -1                  # half a cm
@@ -29,20 +29,21 @@ class MindsStormUtil:
 
 
     @staticmethod
-    def drive_forwards_till_color(precision_module: PrecisionModule, color_sensor: ColorSensor, color_value:int, max_mm: float) -> bool:
+    def drive_forwards_till_color(precision_module: PrecisionModule, color_sensor: ColorSensor, color_value:tuple[int, int, int], max_mm: float) -> bool:
         """
         drive backwards until touching the wall or max_counter of cm
         :param precision_module: Module to use for precisely drive backwards
         :param color_sensor: Sensor to determine abort condition
         :param color_value: Color value to stop at
-        :param max_mm: Maximal amount of mm before aborting and returning false (accurate to 5mm)
+        :param max_mm: Maximal amount of mm before aborting and returning false
         :return: Returns true if color was found, false if max_cm was reached
         """
         ALIGN_FORWARDS = 1  # half a cm
         max_counter = max_mm / ALIGN_FORWARDS
         while True:
-            print(color_sensor.reflection() - color_value)
-            if (color_sensor.reflection() - color_value) < 4:
+            if (((color_sensor.rgb()[0] - color_value[0]) < 4)
+                    and ((color_sensor.rgb()[1] - color_value[1]) < 4)
+                    and ((color_sensor.rgb()[2] - color_value[2]) < 4)):
                 return True
             if max_counter <= 0:
                 return False
