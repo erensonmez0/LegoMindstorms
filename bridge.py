e2002043410655d322579bd2aa51ee6da53da87e
Cornelius <uujud@student.kit.edu>
2025-12-03 16:40:42 +0100

    improve bridge

--- bridge.py
+++ bridge.py
@@ -1,20 +1,20 @@
+from time import sleep
+
 from pybricks.ev3devices import TouchSensor, ColorSensor
 
+from main import BLUE
 from mindstorm_util import MindsStormUtil
 from precision_module import PrecisionModule
 
 
 class Bridge:
     RAMP_UP = 1000
-    TURN_LEFT = 90
-    TURN_RIGHT = -90
+    TURN_LEFT = -90
+    TURN_RIGHT = 90
     BRIDGE_LENGTH = 1250
     RAMP_DOWN = 1000
 
     DISTANCE_TO_BRIDGE_START = 280
-
-    BLUE = 6
-
 
      
     def __init__(self, precision_module:PrecisionModule, touche_sensor:TouchSensor, color_sensor:ColorSensor):
@@ -24,18 +24,19 @@ class Bridge:
 
 
     def align_start(self):
-        self.precision_module.straight_gyro(-200)
+        self.precision_module.straight_gyro(-100)
         self.precision_module.turn_gyro(self.TURN_RIGHT)
         self.precision_module.straight_gyro(- (self.DISTANCE_TO_BRIDGE_START - 50))
 
 
-        MindsStormUtil.drive_backwards_till_wall(self.precision_module, self.touch_sensor, 70)
+        if not MindsStormUtil.drive_backwards_till_wall(self.precision_module, self.touch_sensor, 200):
+            return
 
         # drive to startpoint of ramp
         self.precision_module.straight_gyro(self.DISTANCE_TO_BRIDGE_START)
         self.precision_module.turn_gyro(self.TURN_LEFT)
 
-        MindsStormUtil.drive_forwards_till_color(self.precision_module, self.color_sensor, self.BLUE, 50)
+        MindsStormUtil.drive_forwards_till_color(self.precision_module, self.color_sensor, BLUE, 300)
 
 
         
