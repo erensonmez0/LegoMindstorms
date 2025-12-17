c6d59bc923ec4b5f10daa011d881b4523b12791d
Shrawan Devkota <52040084+SARWANdev@users.noreply.github.com>
2025-12-17 15:14:52 +0100

    Refactor Pringler class methods for improved functionality and adjust parameters for better performance

--- pringler.py
+++ pringler.py
@@ -8,6 +8,7 @@ from precision_module import PrecisionModule
 from bridge import Bridge
 from mindstorm_util import MindsStormUtil
 from config import BLUE
+import time
 
 class Pringler:
     def __init__(self, drive_base:DriveBase, arm_motor:Motor, color_sensor:ColorSensor, ultrasonic_sensor: UltrasonicSensor, precision_module:PrecisionModule,touch_sensor:TouchSensor):
@@ -19,33 +20,28 @@ class Pringler:
         self.touch_sensor = touch_sensor
 
     def grab(self):
-        self.arm_motor.run_target(500,400)
+        self.arm_motor.run_target(500,470)
 
     def initiate_hug_mode(self):
-        self.arm_motor.run_target(500,300)
+        self.arm_motor.run_target(500,380)
 
     def disengage(self):
-        self.arm_motor.run_target(500,-250)
+        self.arm_motor.run_target(500,-200)
 
     def initialise(self):
+        self.drive_base.straight(200)
         distance_from_wall = 100
         #go against wall and set distance
-        self.precision_module.turn_gyro(-90)
+        self.drive_base.turn(-90)
+
         self.precision_module.straight_gyro_with_condition(-200, lambda:(self.touch_sensor.pressed()))
         if self.ultrasonic_sensor.distance()>distance_from_wall:
+            self.precision_module.straight_gyro_with_condition(200, lambda:(self.ultrasonic_sensor.distance()==distance_from_wall))
+        elif self.ultrasonic_sensor.distance()<distance_from_wall:
             self.precision_module.straight_gyro_with_condition(-200, lambda:(self.ultrasonic_sensor.distance()==distance_from_wall))
-        elif self.ultrasonic_sensor.distance()>distance_from_wall:
-            self.precision_module.straight_gyro_with_condition(-200, lambda:(self.ultrasonic_sensor.distance()==distance_from_wall))
-        self.precision_module.turn_gyro(90)
+        self.precision_module.turn_gyro(-90)
             
-    def to_can(self):
-        while True:
-            self.drive_base.drive(140,10)
-            EV3Brick().screen.print(self.ultrasonic_sensor.distance())
-            if self.ultrasonic_sensor.distance()<=30:
-                self.grab()
-                self.drive_base.turn(-90)
-                return
+    
     
     def prep_for_bridge(self):
         DISTANCE_TO_BRIDGE_START = Bridge.DISTANCE_TO_BRIDGE_START
@@ -63,16 +59,17 @@ class Pringler:
 
         
     def run(self):
-        distance_from_end = 800
+        distance_from_end = 650
         distance_from_pring = 40
-        distance_from_square = -300
+        distance_from_square = -550
         self.initialise()
         self.precision_module.straight_gyro_with_condition(10000, lambda:self.ultrasonic_sensor.distance()<=distance_from_end)
         self.precision_module.turn_gyro(45)
         self.initiate_hug_mode()
         self.precision_module.straight_gyro_with_condition(10000, lambda:self.ultrasonic_sensor.distance()<=distance_from_pring)
         self.grab()
-        self.precision_module.straight_gyro(-distance_from_square)
+        time.sleep(1)
+        self.precision_module.straight_gyro(distance_from_square)
         self.disengage()
         self.precision_module.straight_gyro(-100)
         self.drive_base.drive(0,10000)
