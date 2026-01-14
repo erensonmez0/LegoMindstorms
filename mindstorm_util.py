5e4eaafd20c285c40ab3526baaad02687f4eae41
Cornelius <uujud@student.kit.edu>
2026-01-14 13:20:54 +0100

    some doc and code cleanup

--- mindstorm_util.py
+++ mindstorm_util.py
@@ -13,6 +13,7 @@ class MindsStormUtil:
     def check_color(color_sensor:ColorSensor, color_value:tuple[int, int, int], threshold=10) -> bool:
         """
         Check if the given color_value is close to the color given by the color sensor.
+        :param threshold: optional input to use a different threshold for the color matching.
         :param color_sensor: Color sensor to use
         :param color_value: Color value to compare to
         :return: True if values are close to color, False otherwise
@@ -26,9 +27,13 @@ class MindsStormUtil:
     def check_color_euclid(color_sensor:ColorSensor, color_value:tuple[int, int, int], threshold=10) -> bool:
         """
         Check if the given color_value is close to the color given by the color sensor.
+        :param threshold: optional input to use a different threshold for the color matching.
         :param color_sensor: Color sensor to use
         :param color_value: Color value to compare to
         :return: True if values are close to color, False otherwise
         """
-        return math.sqrt((color_sensor.rgb()[0] - color_value[0])**2 + (color_sensor.rgb()[1] - color_value[1])**2+ (color_sensor.rgb()[2] - color_value[2])**2) < threshold
+        return (math.sqrt((color_sensor.rgb()[0] - color_value[0])**2
+                          + (color_sensor.rgb()[1] - color_value[1])**2
+                          + (color_sensor.rgb()[2] - color_value[2])**2)
+                < threshold)
                
